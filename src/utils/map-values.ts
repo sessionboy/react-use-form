755a35985e3da4e9c4bf5aff4ba3854b4f03816a
areschen <ariesjia00@hotmail.com>
2019-02-08 19:59:50 +0800

    init pages

--- src/utils/map-values.ts
+++
@@ -1,5 +0,0 @@
-export const mapValues = (obj, fn) =>
-  Object.keys(obj).reduce((prev, k) => {
-    prev[k] = fn(obj[k], k, obj);
-    return prev;
-  }, {});
--- src/utils/omit.ts
+++
@@ -1,6 +0,0 @@
-export const omit = (obj, keys) => {
-  return Object.keys(obj)
-    .filter((key) => keys.indexOf(key) < 0)
-    .reduce((newObj, key) => Object.assign(newObj, { [key]: obj[key] }), {})
-    ;
-}
